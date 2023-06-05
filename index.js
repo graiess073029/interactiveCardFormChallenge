ea473e4e8f08ff0e557617bfd15ac6dbd15c4a18
Graiess_Youssef <104838584+graiess073029@users.noreply.github.com>
2023-06-05 21:13:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post("/thank.html", (req, res) => {
     
 })
 
-app.use((req,res,next) => res.status(404).json({error : "Page nor Found", code : 404})
+app.use((req,res,next) => res.status(404).json({error : "Page nor Found", code : 404}))
 
 
 app.listen(5500)
