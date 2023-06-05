fd9dec0e3d2345c72a65531222df5f61e5506df5
Graiess_Youssef <104838584+graiess073029@users.noreply.github.com>
2023-06-05 21:10:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 app.use(express.static("public"))
+app.get("/",(req,res)=> res.redirect("/index.html"))
 app.get("/index.html", (req, res) => res.status(200).sendFile(path.join(__dirname + "/public/views/index.html")))
 app.post("/thank.html", (req, res) => {
     let name = req.body.name
@@ -72,5 +73,7 @@ app.post("/thank.html", (req, res) => {
     
 })
 
+app.use((req,res,next) => res.status(404).json({error : "Page nor Found", code : 404})
 
-app.listen(5500, () => console.log("Worked"))
+
+app.listen(5500)
