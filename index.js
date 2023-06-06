aacb235e44dcdf0562f8e5988fe89066ea9f3154
Graiess_Youssef <104838584+graiess073029@users.noreply.github.com>
2023-06-06 09:39:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 const app = express()
 
-app.use(express.static(path.join(__dirname + "public")))
+app.use(express.static(path.join(__dirname + "/public/")))
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 app.get("/",(req,res)=> res.redirect("/index.html"))
